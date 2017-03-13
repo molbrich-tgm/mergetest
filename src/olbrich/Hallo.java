0d87435c887cdcf01d8f0e2368c3f9bd22b0ada8
molbrich <molbrich@student.tgm.ac.at>
2017-03-13 10:24:37 +0100

    Mit !!

--- src/olbrich/Hallo.java
+++ src/olbrich/Hallo.java
@@ -3,11 +3,11 @@ package olbrich;
 public class Hallo {
 
 	public static void gutenTag(){
-		System.out.println("Guten Tag.");
+		System.out.println("Guten Tag!!!!");
 	}
 	
 	public static void byBy(){
-		System.out.println("Auf Wiedersehen.");
+		System.out.println("Auf Wiedersehen!!!");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
